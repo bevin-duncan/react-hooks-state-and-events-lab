e420f16ea7b5393b02870a1be47c44cf7c79ce88
Bevin Duncan <bevin.duncan@gmail.com>
2022-02-24 20:12:22 -0800

    Completed Assignment

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [selectedCat , setSelectedCat] = useState("All")
+
+function handleCatChange(e){
+      setSelectedCat(e.target.value)
+}
+
+const displayItems = items.filter((item) => {
+  if (selectedCat === "All") return true;
+
+  return item.category === selectedCat;
+});
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleCatChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +24,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {displayItems.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
